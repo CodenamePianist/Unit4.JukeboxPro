0fcb585d010d1aa7652196c1414f87e386711156
Brandon Hamilton <bham2015@gmail.com>
2024-10-16 15:17:22 -0500

    Wrote token-checking middleware in the auth.js file.

--- api/auth.js
+++ api/auth.js
@@ -1,2 +1,29 @@
 const express = require("express");
 const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+async function createToken(id) {
+  jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" });
+}
+
+const prisma = require("../prisma");
+
+// Token-checking middleware
+router.use(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.slice(7);
+  if (!token) return next();
+
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { id },
+    });
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
