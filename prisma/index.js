193db1088665932af40119af441a87dd4771c77d
Brandon Hamilton <bham2015@gmail.com>
2024-10-16 14:59:02 -0500

    Set up the index.js file.

--- prisma/index.js
+++ prisma/index.js
@@ -1,3 +1,29 @@
+const { bcrypt } = require("bcrypt");
+
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async register(username, password) {
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newUser = await prisma.user.create({
+          data: {
+            username,
+            password: hashPassword,
+          },
+        });
+        return newUser;
+      },
+      async login(username, password) {
+        const user = await prisma.user.findUniqueOrThrow({
+          where: { username },
+        });
+        const valid = await bcrypt.compare(password, hashPassword);
+        if (!valid) throw Error("Invalid password");
+        return user;
+      },
+    },
+  },
+});
+
 module.exports = prisma;
